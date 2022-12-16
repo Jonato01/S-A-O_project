9d957b0609c9c0e0c97aa4efadac9037e3c47045
AkaiToKuroi <nathan.actis@gmail.com>
2022-12-16 13:37:19 +0100

    Cominciato a lavorare sulla shm

--- master.c
+++ master.c
@@ -8,8 +8,7 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/sem.h>
-
-
+#include "shm.h"
 
 #define _GNU_SOURCE
 #define SO_NAVI 20
@@ -20,16 +19,7 @@
 #define SO_MIN_VITA 2 
 #define S0_MAX_VITA 10
 #define SO_LATO 100.00    //Dimensioni della mappa in km
-struct shared_data {
-	/* index where next write will happen */
-	unsigned long cur_idx;
-	/*
-	 * vector that contains the PIDS of the process which accessed
-	 * the shared memory
-	 */
-	
-	int j;  
-};
+
  struct merce{
     int id;
     char* nome;
@@ -37,15 +27,19 @@ struct shared_data {
     int size;
     int vita;
 };
-int main(){
-    int mem_id;
-    struct shared_data * sh_mem;
-    double map[SO_SIZE][SO_SIZE];
-     mem_id = shmget ( getpid(), sizeof(*sh_mem), 0600 | IPC_CREAT );
-    sh_mem = shmat(mem_id, NULL, 0);
-    sh_mem->cur_idx=0;
-    sh_mem->j= 100;
 
+int main(){
+    int mem_id, sem_id;
+    struct shared_data * sh_mem;
+    struct sembuf sops;
+    double map[SO_SIZE][SO_SIZE];
+
+    mem_id = shmget (getpid(), sizeof(*sh_mem), 0600 | IPC_CREAT );
+    sh_mem = shmat(mem_id, NULL, 0);
+
+    sh_mem->cur_idx=0;
+    sh_mem->map_size = SO_SIZE;
+    sh_mem->j= 100;
 
     shmdt ( sh_mem );
     shmctl ( mem_id , IPC_RMID , NULL );
---
+++ porto.c
@@ -0,0 +1,12 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include "shm.h"
+
+#define SO_BANCHINE
+
+double coordinates[2];
+
+int main(){
+
+}
