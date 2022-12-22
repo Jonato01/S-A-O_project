cdc8b82c1cf5a1bb58321e7a3570cc88e3d9caec
AkaiToKuroi <nathan.actis@gmail.com>
2022-12-23 00:07:06 +0100

    Completati metodi getdest e getpart

--- nave.c
+++ nave.c
@@ -26,7 +26,7 @@ void swap(int* xp, int* yp)
     *yp = temp;
 }
 
-void ordinaporti(struct coord){
+void ordinaporti(struct coordinates coord){
     int i;
     int j;
     bool swapped;
@@ -34,67 +34,64 @@ void ordinaporti(struct coord){
         ord[i] = sh_mem->porti[i].idp;
     }
     for (i = 0; i < SO_PORTI - 1; i++){
-        swappxed = false;
+        swapped = false;
         for (j = 0; j < SO_PORTI - i - 1; j++)
-            if (DISTANCE(sh_mem->porti[ord[j]].coord,coord) > DISTANCE(sh_mem->porti[ord[j+1]].coord,coord)){
+            if (DISTANCE(sh_mem->porti[ord[j]].coord, coord) > DISTANCE(sh_mem->porti[ord[j+1]].coord, coord)){
                 swap(&ord[j], &ord[j + 1]);
                 swapped = true;
             }
         if(!swapped)
             break;
     }
-    for(i = 0; i < SO_PORTI; i++){
+    /*for(i = 0; i < SO_PORTI; i++){
         printf("%d ", ord[i]);
     }
+    printf("\n");*/
 }
 
-bool cerca_merce(int idp)
-{
+int contains(int portoid, int merceid){
     int i;
-    bool ce=false;
-    for(i=0;!ce && i<MERCI_RIC_OFF;i++)
-    if(sh_mem->porti[idp].off[i].id==merci_ric[i])
-    ce=!ce;
 
-    
-    return ce;
+    for(i = 0; i < MERCI_RIC_OFF; i++){
+        if(sh_mem->porti[portoid].off[i].id == merceid){
+            return i;
+        }
+    }
+    return -1;
 }
+
 int getpart()
 {
     int x = -1;
+    int y;
     int i;
     int j;
     bool flag = false;
-    LOCK
-    
-        for(i = 0; i < SO_PORTI; i++){
-            for(j = 0; j < MERCI_RIC_OFF && merci_ric[j]!=0 ; j++){
-                    if(!sh_mem->porti[ord[i]].off[j].pre){
-                        flag = true;
-                        sh_mem->porti[ord[i]].off[j].pre = true;
-
-                    }
+    LOCK   
+    for(i = 0; i < SO_PORTI; i++){
+        for(j = 0; j < MERCI_RIC_OFF; j++){
+                y = contains(ord[i], merci_ric[j].id);
+                if((y > -1) && !sh_mem->porti[ord[i]].off[y].pre && merci_ric[j].pre){
+                    flag = true;
+                    sh_mem->porti[ord[i]].off[y].pre = true;
+                    printf("Nave %d: prenotata merce %d da caricare al porto %d (carico)\n", barchetta.idn, sh_mem->porti[ord[i]].off[y].id, ord[i]);
                 }
-            
-                if(flag){
-                    x = ord[i];
-                    break;
             }
-        }
         
+        if(flag){
+            x = ord[i];
+            break;
+        }
+    }
+    if(!flag){
+        printf("Nave %d: nessun porto utilizzabile! (carico)\n", barchetta.idn);
+    }     
     UNLOCK
-    printf("");
-    barchetta.idp_dest=x;
+    printf("\n");
+    return x;
 }
 
-
-
-
-
-
-
-
-void getdest()
+int getdest()
 {
     int x = -1;
     int i;
@@ -103,15 +100,16 @@ void getdest()
     LOCK
     if(barchetta.carico_pre < SO_CAPACITY){
         for(i = 0; i < SO_PORTI; i++){
-            for(j = 0; j < MERCI_RIC_OFF && sh_mem->porti[ord[i]].off[j].size + barchetta.carico_pre < SO_SIZE; j++){
-                if(!sh_mem->porti[ord[i]].off[j].pre){
+            for(j = 0; j < MERCI_RIC_OFF && sh_mem->porti[ord[i]].ric[j].size + barchetta.carico_pre < SO_CAPACITY; j++){
+                if(!sh_mem->porti[ord[i]].ric[j].pre){
                     flag = true;
-                    sh_mem->porti[ord[i]].off[j].pre = true;
-                    merci_ric[j]=sh_mem->porti[ord[i]].off[j];
-                    barchetta.carico_pre += sh_mem->porti[ord[i]].off[j].size;
-                    printf("Nave %d: prenotata merce %d dal porto %d\n", barchetta.idn, sh_mem->porti[i].off[j].id, ord[i]);
-                    }
+                    sh_mem->porti[ord[i]].ric[j].pre = true;
+            /*!!!j*/merci_ric[j]=sh_mem->porti[ord[i]].ric[j];
+                    merci_ric[j].pre = true;
+                    barchetta.carico_pre += sh_mem->porti[ord[i]].ric[j].size;
+                    printf("Nave %d: prenotata merce %d da consegnare al porto %d (scarico)\n", barchetta.idn, sh_mem->porti[ord[i]].ric[j].id, ord[i]);
                 }
+            }
             
             if(flag){
                 x = ord[i];
@@ -119,11 +117,13 @@ void getdest()
             }
         }
         if(!flag)
-            printf("Nave %d: nessun porto utilizzabile! (carico)\n", barchetta.idn);
+            printf("Nave %d: nessun porto utilizzabile! (scarico)\n", barchetta.idn);
+        else
+            printf("Totale carico: %f su %d\n", barchetta.carico_pre, SO_CAPACITY);
     }else
         printf("Nave %d piena!\n", barchetta.idn);
     UNLOCK
-    barchetta.idp_dest=x;
+    return x;
 }
 
 void gennave()
@@ -146,12 +146,15 @@ int main (int argc, char * argv[]){
     mem_id = shmget(getppid(), sizeof(*sh_mem), 0600);
     sh_mem = shmat(mem_id, NULL, 0);
     /*TEST ERROR*/
+
     gennave();
+
     ordinaporti(barchetta.coord);
-    barchetta.idp_dest=getpart();
+    barchetta.idp_dest = getdest();
     ordinaporti(sh_mem->porti[barchetta.idp_dest].coord);
-    if(barchetta.idp_dest != -1)
-        printf("Mi dirigo verso il porto %d\nDistanza: %f\n\n", barchetta.idp_dest, DISTANCE(sh_mem->porti[barchetta.idp_dest].coord, barchetta.coord));
+    barchetta.idp_part = getpart();
+    if(barchetta.idp_part != -1)
+        printf("Mi dirigo verso il porto %d\nDistanza: %f\n\n", barchetta.idp_part, DISTANCE(sh_mem->porti[barchetta.idp_part].coord, barchetta.coord));
 
     /*CONTINUARE*/
 
