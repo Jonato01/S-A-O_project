1472feecd43eca9f1fb006b41709a739f14b02f1
Gionato65437 <renatogioana63@gmail.com>
2023-01-11 12:21:34 +0100

    funziona solo da replicare negli altri metodi

--- master.c
+++ master.c
@@ -10,15 +10,16 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <signal.h>
+#include <sys/stat.h>
 #include <sys/sem.h>
 #include "shm.h"
 #include "var.h"
 #include <stdbool.h>
 #include <sys/wait.h>   
 struct sembuf sops;
-int sem_id; int* mem_id; int banchine;
+int sem_id; int mem_id; int banchine;
 struct shared_data * sh_mem;
-
+const int j=sizeof(struct shared_data)+(sizeof(struct porto)+sizeof(struct merce)*2*MERCI_RIC_OFF*sizeof(pid_t))*SO_PORTI+(sizeof(struct merce))*SO_MERCI+sizeof(pid_t)*SO_NAVI;
 pid_t *porti;
 pid_t *navi;
 pid_t meteo;
@@ -35,9 +36,13 @@ void fine_sim(int signal)
         if(n<SO_PORTI)
         kill(porti[n],SIGINT);
     }
-    shmdt ( sh_mem );
-    printf("\nDeleting shm with id \n");
-    shmctl(mem_id[0] , IPC_RMID , NULL);
+    if(munmap(sh_mem, j) == -1) {
+        perror("Error unmapping shared memory");
+    }
+    if(close(mem_id) == -1) {
+        perror("Error closing shared memory");
+    }
+    
     printf("Deleting sem with id %d\n",sem_id);
     semctl(sem_id, 0, IPC_RMID);
     semctl(banchine, 0, IPC_RMID);
@@ -111,12 +116,13 @@ void genporti()
 
 int main(int args,char* argv[]){
     
-    int i; int k; int j;
+    int i; int k; 
     struct sigaction sa;
+    
     srand(getpid());
     setvar();
     i=SIZEMEM;
-    mem_id=calloc(5000,sizeof(int));
+    
     printf("%d %d %d\n", SO_NAVI, SO_PORTI, SO_MERCI);
     sa.sa_handler=fine_sim;
     sigaction(SIGINT, &sa, NULL);
@@ -134,22 +140,42 @@ int main(int args,char* argv[]){
     semctl(sem_id, 0, SETVAL, 1);
     resetSems(sem_id);
     mem_id=shm_open("Sh_mem_prog", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
-    if (shm_fd == -1) {
-        perror("Error opening shared memory")  
+    if (mem_id  == -1) {
+        perror("Error opening shared memory"); 
         exit(EXIT_FAILURE);
     }
-    if (ftruncate(mem_id,sizeof(struct shared_data)+(sizeof(struct porto)+sizeof(struct merce)*2*MERCI_RIC_OFF*sizeof(pid_t))*SO_PORTI+(sizeof(struct merce))*SO_MERCI+sizeof(pid_t)*SO_NAVI) == -1) {
-        
+    if (ftruncate(mem_id,j) == -1) {
+        perror("error on ftruncate sh_mem");
+        exit(1);
     }
-
-
+    sh_mem = mmap(NULL,j, PROT_READ | PROT_WRITE, MAP_SHARED, mem_id, 0);
+    if (sh_mem == MAP_FAILED) {
+        perror("Error mapping shared memory: "); 
+        exit(1);
+    }
+    sh_mem->navi_in_transito=mmap(NULL,sizeof( pid_t)*SO_NAVI, PROT_READ | PROT_WRITE, MAP_SHARED| MAP_ANONYMOUS, -1, 0);
+    sh_mem->merci=mmap(NULL,sizeof(struct merce)*SO_MERCI, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    
+    sh_mem->porti=mmap(NULL,sizeof(struct porto)*SO_PORTI, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    
+    for(i=0;i<SO_PORTI;i++)
+    {
+        sh_mem->porti[i].off=mmap(NULL,sizeof(struct merce)*MERCI_RIC_OFF, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+        sh_mem->porti[i].ric=mmap(NULL,sizeof(struct merce)*MERCI_RIC_OFF, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+        for(k=0;k<MERCI_RIC_OFF;k++)
+        {
+            sh_mem->porti[i].off[k].pid_navi=mmap(NULL,sizeof(pid_t)*SO_NAVI, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+            sh_mem->porti[i].ric[k].pid_navi=mmap(NULL,sizeof(pid_t)*SO_NAVI, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+        }
+    } 
     printf("Creating shm with id: %d\nCreating sem with id:%d\n\n", 5, sem_id);
     /*creazione merci*/
     LOCK
-    printf("ciao\n");
+    
     for(i=0;i<SO_MERCI;i++)
     {
         sh_mem-> merci[i].id=i;
+        
         sh_mem-> merci[i].size=rand()%(int)SO_SIZE+1;
         sh_mem-> merci[i].vita=rand()%(int)(S0_MAX_VITA-SO_MIN_VITA+1)+SO_MIN_VITA;
         sh_mem-> merci[i].num=0;
